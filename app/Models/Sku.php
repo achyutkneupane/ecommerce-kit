63ee33e4ce0a3dbf5b04750810cbd95305066d50
achyutkneupane <achyutkneupane@gmail.com>
2026-03-20 01:20:54 +0545

    feat: adds price accessor to Sku model for handling currency conversion

--- app/Models/Sku.php
+++ app/Models/Sku.php
@@ -5,9 +5,18 @@ declare(strict_types=1);
 namespace App\Models;
 
 use AchyutN\LaravelHelpers\Models\MediaModel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sku extends MediaModel
 {
     use SoftDeletes;
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn (int $value) => $value / 100,
+            set: fn (float|int $value) => (int) ($value * 100),
+        );
+    }
 }
